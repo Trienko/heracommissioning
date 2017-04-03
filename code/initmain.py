b0357b922f65fcc454e06e4f64b3c011d082f373
trienko <trienkog@gmail.com>
2017-04-03 11:33:34 +0200

    started initial task file

---
+++ code/initmain.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pylab as plt
+
+class inittasks():
+
+      def __init__(self):
+          pass
+
+if __name__ == "__main__":
+   inittasks_object = inittasks()
+   print inittasks
