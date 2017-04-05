007f1b6c2a6ba08fe08175f89075ad503d87f87c
trienko <trienkog@gmail.com>
2017-04-05 18:24:39 +0200

    added cal code for ms with gc at cen

--- code/redpipe.py
+++ code/redpipe.py
@@ -8,8 +8,8 @@ FLAG_SPW_STRING = '0:0~140;379~387;768~770;851~852;901~1023'
 FLAG_ANT_STRING = '81;82;113'
 SGR_STR = '17:45:40.0'
 SGR_FLOAT = (17.0/24 + 45.0/60 + 40.0/3600)*(pi/12)
-BANDBASS_GC_CAL = ''
-
+BANDBASS_GC_CAL_TABLE = ''
+POINT_SOURCE_MODEL = 'point_source_model.cl'
 
 class redpipe():
 
@@ -23,6 +23,32 @@ class redpipe():
           it.CASA_WRAPPER(task="flagdata",options=options)
           print os.getcwd()
           command = "casa -c flagdata_script.py --nogui --nologfile --log2term"
+          print("CMD >>> "+command)
+          os.system(command) 
+          command = "rm ipython*.log"
+          print("CMD >>> "+command)
+          os.system(command)
+
+      #####################################
+      #CASA wrapper around ft 
+      #####################################
+      def ft_wrapper(self,options={}):
+          it.CASA_WRAPPER(task="ft",options=options)
+          print os.getcwd()
+          command = "casa -c ft_script.py --nogui --nologfile --log2term"
+          print("CMD >>> "+command)
+          os.system(command) 
+          command = "rm ipython*.log"
+          print("CMD >>> "+command)
+          os.system(command)
+
+      #####################################
+      #CASA wrapper around bandpass 
+      #####################################
+      def bandpass_wrapper(self,options={}):
+          it.CASA_WRAPPER(task="bandpass",options=options)
+          print os.getcwd()
+          command = "casa -c bandpass_script.py --nogui --nologfile --log2term"
           print("CMD >>> "+command)
           os.system(command) 
           command = "rm ipython*.log"
@@ -101,9 +127,10 @@ class redpipe():
           return msname
 
       def bandpass_gc(self):
-          gc_name = self.print_lst(print_values=True) #print lst flips between code and data dir already needs to be placed first
+          gc_name = self.print_lst(print_values=False) #print lst flips between code and data dir already needs to be placed first
           os.chdir(it.PATH_DATA)
           
+          #CREAT POINT SOURCE MODEL AT THE PS OF GALACTIC CENTER
           if not os.path.isfile('point_source_model.cl'):
              command = "casa -c create_ps.py --nogui --nologfile --log2term"
              print("CMD >>> "+command)
@@ -111,26 +138,32 @@ class redpipe():
              command = "rm ipython*.log"
              print("CMD >>> "+command)
              os.system(command)
-
-          cl.addcomponent(flux=1, fluxunit='Jy', polarization='Stokes',dir='J2000 17h45m40.0s -29d00m28s', shape='point')
-          complist_name = 'point_source_model.cl'
-          cl.rename(complist_name)
-          cl.close()
-
-          ft(vis='zen.2457545.48011.xx.HH.uvcU.ms',complist='point_source_model.cl',usescratch=True)
-          bandpass_table = 'b_2457545.48011.cal'
-	  bandpass_selfcal_table = 'b_selfcal_2457545.48011.cal'
-	  bandpass(vis='zen.2457545.48011.xx.HH.uvcU.ms', solint='inf', combine='scan', caltable=bandpass_table)
-          bandpass(vis='zen.2457545.48011.xx.HH.uvcU.ms', solint='inf',combine='scan', gaintable = bandpass_table, caltable=bandpass_selfcal_table)
-
-          	
           
-                
+          #FT POINT SOURCE MODEL
+          options={}          
+          options["vis"]=gc_name
+          options["complist"]=POINT_SOURCE_MODEL
+          options["usescratch"]=True
+          self.ft_wrapper(options=options)
 
+          gc_name_split = gc_name.split('.')
+          gc_jd = gc_name_split[1]+'.'+gc_name_split[2]
+          
+          BANDBASS_GC_CAL_TABLE = 'b_'+gc_jd+'.cal'
+
+          #BANDPASS CALIBRTION OF GC-MS WITH PS AT GALACTIC CENTER
+          options={}
+          options["vis"]=gc_name
+          options["solint"]='inf'
+          options["combine"]='scan'
+          options["caltable"] = BANDBASS_GC_CAL_TABLE 
+          self.bandpass_wrapper(options=options)
+          
 if __name__ == "__main__":
    #main(sys.argv[1:])
    red_object = redpipe()
    print red_object.print_lst(print_values=True)
+   red_object.bandpass_gc()
    #red_object.flag_basic_all()
    #plot_object = plotutilities()
 
