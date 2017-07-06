2f26126755ec2052d38ab38f14ea9464996371dd
trienko <trienkog@gmail.com>
2017-07-06 14:51:17 +0200

    added new files

--- code/short_pipe.py
+++ code/short_pipe.py
@@ -10,13 +10,28 @@ if __name__ == "__main__":
    red_object = red.redpipe()
    abs_object = absf.absflux()
 
+   inittasks_object.split_ms()
+   #inittasks_object.compute_time_str()
+   #inittasks_object.create_time_pickle()
+
+   '''
    inittasks_object.add_uv_tracks()
-   inittasks_object.miriad_to_uvfits_rid()
+   #inittasks_object.miriad_to_uvfits_rid()
+   
+   command = "python initscript.py --sudo_miriad_to_uvfits"
+   print("CMD >>> "+command)
+   os.system(command)
+      
    inittasks_object.remove_ms()
    inittasks_object.uv_fits_to_ms()
    inittasks_object.swap_antenna()
-   inittasks_object.apply_flags()
-   red_object.applycal_gc_all(delay=True)
+   #inittasks_object.apply_flags()
+
+   command = "python redpipe.py -d --flag_all_basic --bandpass_gc --apply_cal_gc_all"
+   print("CMD >>> "+command)
+   os.system(command)
+   
+   #red_object.applycal_gc_all(delay=True)
    abs_object.apply_c()
    inittasks_object.ms_to_uv_fits()
    inittasks_object.uvfits_to_miriad()
@@ -36,4 +51,4 @@ if __name__ == "__main__":
 #   UV = UVData()
 #   UV.read_uvfits(uvfits,run_check=False,run_check_acceptability=False)
 #   UV.write_miriad(miriad_file,run_check=False, run_check_acceptability=False,clobber=True)
-
+    '''
