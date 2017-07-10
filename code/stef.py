b85dc4ea79e0c9b0f1f9f9eb2b7c0a1e8e9c2497
trienko <trienkog@gmail.com>
2017-07-10 16:50:07 +0200

    crude stefcal implementation

--- code/stef.py
+++ code/stef.py
@@ -4,6 +4,8 @@ import os,sys
 import glob
 import numpy as np
 import pylab as plt
+import time
+import matplotlib as mpl
 
 
 class redundant_stefcal():
@@ -152,7 +154,7 @@ class redundant_stefcal():
               #print "PQ = ",PQ    
           return PQ 
 
-      def convert_y_to_M(PQ,y,N):
+      def convert_y_to_M(self,PQ,y,N):
     
           M = np.zeros((N,N),dtype=complex)
           for i in xrange(len(y)):
@@ -168,22 +170,40 @@ class redundant_stefcal():
           #from IPython import embed; embed() 
           return M  
 
-      def redundant_StEFCal(D,phi,tau=1e-3,alpha=0.3,max_itr=1000,PQ=None):
+      def convert_M_to_y(self,PQ,M):
+          L = len(PQ.keys())
+          y = np.zeros((L,),dtype=complex)    
+
+          for i in xrange(L):
+              pq = PQ[str(i)]
+              #print "pq = ",pq
+              temp_len = len(pq)            
+              for k in xrange(len(pq)):
+                  p = pq[k][0]
+                  q = pq[k][1]
+                  y[i] = y[i] + M[p,q]             
+              y[i] = y[i]/temp_len
+          #from IPython import embed; embed() 
+          return y  
+
+      def redundant_StEFCal(self,D,phi,tau=1e-3,alpha=0.3,max_itr=10,PQ=None):
           converged = False
           N = D.shape[0]
           D = D - D*np.eye(N)
           L = np.amax(phi)
           temp =np.ones((D.shape[0],D.shape[1]) ,dtype=complex)
     
-          g_temp = np.ones((N,),dtype=complex)
-          y_temp = np.ones((L,),dtype=complex)
-          z_temp = np.hstack([g_temp,y_temp])
-
           error_vector = np.array([])
 
           #EXTRACT BASELINE INDICES FOR EACH REDUNDANT SPACING
           if PQ is not None:
-             PQ = create_PQ(phi,L)
+             PQ = self.create_PQ(phi,L)
+
+          g_temp = np.ones((N,),dtype=complex)
+          #y_temp = np.ones((L,),dtype=complex)
+          y_temp = self.convert_M_to_y(PQ,D)
+          z_temp = np.hstack([g_temp,y_temp])
+
 
           start = time.time() 
     
@@ -192,7 +212,7 @@ class redundant_stefcal():
               y_old = np.copy(y_temp)
               z_old = np.copy(z_temp)
  
-              M = convert_y_to_M(PQ,y_old,N) #CONVERT y VECTOR TO M matrix
+              M = self.convert_y_to_M(PQ,y_old,N) #CONVERT y VECTOR TO M matrix
     
               #plt.imshow(np.absolute(M))
               #plt.show()
@@ -236,15 +256,15 @@ class redundant_stefcal():
           stop = time.time()
           G = np.dot(np.diag(g_temp),temp)
           G = np.dot(G,np.diag(g_temp.conj()))  
-          M = convert_y_to_M(PQ,y_temp,N)         
+          M = self.convert_y_to_M(PQ,y_temp,N)         
 
           return z_temp,converged,G,M,start,stop,i,error_vector
 
       def find_indices(self,a,b):
           if a < b:
-             return a,b
+             return a,b,False
           else:
-             return b,a 
+             return b,a,True 
  
       def read_in_D(self,ms_file):
           os.chdir(it.PATH_DATA)
@@ -252,7 +272,7 @@ class redundant_stefcal():
           B = (N**2 + N)/2 #ASSUMING THE FILE CONTAINS AUTOCORRELATIONS
 
           t=table(ms_file)
-          data = t.getcol("DATA")
+          data = t.getcol("CORRECTED_DATA")
           flag = t.getcol("FLAG")
           flag_row = t.getcol("FLAG_ROW")
           ant1 = t.getcol("ANTENNA1")
@@ -271,9 +291,15 @@ class redundant_stefcal():
 
           for k in xrange(N):
               for j in xrange(k,N):
-                  p,q = self.find_indices(it.ANT_ID[k],it.ANT_ID[j])
+                  p,q,greater = self.find_indices(it.ANT_ID[k],it.ANT_ID[j])
                   temp_indx = np.logical_and(ant1==p,ant2==q)
-                  data_mat[k,j,:,:] = data[temp_indx,:,0]
+                  if greater:
+                     data_mat[k,j,:,:] = np.conjugate(data[temp_indx,:,0])
+                     data_mat[j,k,:,:] = data[temp_indx,:,0]
+                  else:
+                     data_mat[k,j,:,:] = data[temp_indx,:,0]
+                     data_mat[j,k,:,:] = np.conjugate(data[temp_indx,:,0])
+
                   flag_mat[k,j,:,:] = flag[temp_indx,:,0]
                   flag_row_mat[k,j,:] = flag_row[temp_indx]
                   indx_2d = indx_1d[temp_indx] 
@@ -284,14 +310,98 @@ class redundant_stefcal():
 
           os.chdir(it.PATH_CODE)
           return data_mat,flag_mat,flag_row_mat,indx_2d
+ 
+      def apply_redundant_stefcal(self,ms_file):
+          os.chdir(it.PATH_DATA)
+          data_mat,flag_mat,flag_row_mat, indx_2d = self.read_in_D(ms_file)
+          ant = self.hex_grid(hex_dim=2,l=14.6)
+          #plt.plot(ant[:,0],ant[:,1],'bo')
+          #plt.show()
+          phi,zeta,L = self.calculate_phi(ant[:,0],ant[:,1])
+          plt.imshow(phi)
+          plt.show()
+          PQ = self.create_PQ(phi,L)
+          #print "PQ = ",PQ
+          plt.imshow(np.absolute(data_mat[:,:,25,701]-data_mat[:,:,25,701]*np.eye(19)))
+          plt.show()
+          z_temp,converged,G,M,start,stop,i,error_vector = self.redundant_StEFCal(D=data_mat[:,:,25,701],phi=phi,tau=1e-9,alpha=1.0/3.0,max_itr=1000,PQ=PQ)
+          print "converged = ",converged
+          plt.imshow(np.absolute(M))
+          plt.show()
+          plt.imshow(np.absolute(data_mat[:,:,25,701]*G**(-1)-data_mat[:,:,25,701]*np.eye(19)*G**(-1)))
+          plt.show()
+
+          plot_before_after_cal_per_t_f(data_mat[:,:,25,701],G,phi)
+          os.chdir(it.PATH_CODE)
+
+def find_color_marker(index_value):
+    color = np.array(['b','g','r','c','m','y','k'])
+    marker = np.array(['o','v','^','<','>','8','s','p','*','H','+','x','D'])
+    index_value = index_value-1
+    counter = 0
+    c_final = None
+    m_final = None
+    for k in xrange(len(marker)):
+        for l in xrange(len(color)):
+            if counter == index_value:
+               c_final = color[l]
+               m_final = marker[k]
+               break
+            counter = counter+1
+        else:
+            continue  # executed if the loop ended normally (no break)
+        break  # executed if loop ended through break
+    #print "c_final = ",c_final
+    #print "m_final = ",m_final
+
+    if c_final is None:
+       i_c = np.random.randint(low=0,high=len(color))
+       i_m = np.random.randint(low=0,high=len(marker)) 
+       c_final = color[i_c]
+       m_final = color[i_m]
+    return c_final,m_final 
+
+def plot_before_after_cal_per_t_f(D,G,phi):
+        mpl.rcParams['xtick.labelsize'] = 20 
+        mpl.rcParams['ytick.labelsize'] = 20 
+        #mpl.rcParams['ylabel.labelsize'] = 22  
+        #mpl.rcParams['xlabel.labelsize'] = 22 
+        #mpl.rcParams['title.labelsize'] = 22
+        D_new = D[:,:]
+        G_new = G[:,:]
+        D_cal = D_new*G_new**(-1)
+        for i in xrange(D_new.shape[0]):
+            for j in xrange(i+1,D_new.shape[1]):
+                ind = phi[i,j]
+                #print "ind = ",ind
+                c_final,m_final = find_color_marker(ind)
+                plt.plot(D_new[i,j].real,D_new[i,j].imag,c_final+m_final,ms=10)
+        plt.axis("equal")
+        plt.xlabel("real",fontsize=15)
+        plt.ylabel("imag",fontsize=15)
+        plt.title("Before Calibration",fontsize=15)
+        plt.show()        
+
+        for i in xrange(D_cal.shape[0]):
+            for j in xrange(i+1,D_cal.shape[1]):
+                ind = phi[i,j]
+                c_final,m_final = find_color_marker(ind)
+                plt.plot(D_cal[i,j].real,D_cal[i,j].imag,c_final+m_final,ms=10)
+        plt.axis("equal")
+        plt.ylabel("imag",fontsize=15)
+        plt.xlabel("real",fontsize=15)
+        plt.title("After Calibration",fontsize=15)
+        plt.show()
 
 if __name__ == "__main__":
    red_cal = redundant_stefcal()
    os.chdir(it.PATH_DATA)
-   file_names = glob.glob("*uvcU.ms")
+   file_names = glob.glob("*uvcUC.ms")
    os.chdir(it.PATH_CODE)
-   data_mat,flag_mat,flag_row_mat, indx_2d = red_cal.read_in_D(file_names[0])
+   red_cal.apply_redundant_stefcal(file_names[0])
 
-   plt.plot(data_mat[0,3,30,:])
-   plt.show()
+   #data_mat,flag_mat,flag_row_mat, indx_2d = red_cal.read_in_D(file_names[0])
+
+   #plt.plot(data_mat[0,3,30,:])
+   #plt.show()
 
