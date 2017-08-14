d48b1c5292282a2a162e6fc12256800b75ad07fa
trienko <trienkog@gmail.com>
2017-08-14 13:37:52 +0200

    new time_func.py

--- code/time_func.py
+++ code/time_func.py
@@ -1,8 +1,9 @@
 def time_convert(mytime, myunit='s'):
-    if type(mytime).__name__ <> 'list': mytime=[mytime]
+    #if type(mytime).__name__ <> 'list': mytime=[mytime]
     myTimestr = []
-    for time in mytime:
-        q1=qa.quantity(time,myunit)
+    #for time in mytime:
+    for k in xrange(len(mytime)):  
+        q1=qa.quantity(mytime[k],myunit)
         time1=qa.time(q1,form='ymd')
         myTimestr.append(time1)
     return myTimestr
