bdb959c765e4f1e6fb1e92b5f8011e8f1fcb93d8
trienko <trienkog@gmail.com>
2017-08-17 09:40:53 +0200

    added Abhik's flagging

--- code/redpipe.py
+++ code/redpipe.py
@@ -153,9 +153,175 @@ class redpipe():
           print("CMD >>> "+command)
           os.system(command)
 
-      def flag_basic_all(self):
+      #################    
+      # Added
+      #################
+      ######################################################################
+      # Find redundant antennas & the corresponding visibilities for all the redundant groups
+      #####################################################################
+      def find_redundant_groups_all(self):
+          ant_id = np.array([80,104,96,64,53,31,65,88,9,20,89,43,105,22,81,10,72,112,97])
+          ant_strcom = []
+          red_sp = 14.6 # redundant spacing in m  
+          ant = self.hex_grid(hex_dim=2,l=red_sp) # GENERAT HEX-19 LAYOUT
+          phi,zeta,L = self.calculate_phi(ant[:,0],ant[:,1])		              
+          for i in xrange(1,L+1): # LOOP THROUGH ALL REDUNDANT GROUPS
+              ant_str = ""
+              for k in xrange(len(ant[:,0])):
+                  for j in xrange(k+1,len(ant[:,0])): #LOOP THROUGH ALL ANTENNA PAIRS  
+                      if (phi[k,j] == i):
+                          p = ant_id[k] + 1 #TAKES ANT NAME NOT ID (NAME = ID + 1)
+                          q = ant_id[j] + 1 #SUBSTITUTE THE ID GIVEN BY HEX DIAGRAM ON HERA WIKI
+                          if p < q:
+                              ant_str = ant_str + str(p)+"&"+str(q)+";"
+                          else:
+                              ant_str = ant_str + str(q)+"&"+str(p)+";" 
+              ant_str = ant_str.rstrip('\;') # take out the ; character from right side
+              print ant_str
+              ant_strcom.append(ant_str)
+      
+          return ant_strcom    
+
+      ######################################
+      #Generates the antenna positions for a hexagonal grid
+      ######################################
+      def hex_grid(self,hex_dim,l):
+          hex_dim = int(hex_dim)
+          side = int(hex_dim + 1)
+          ant_main_row = int(side + hex_dim)
+        
+          elements = 1
+
+          #SUMMING ANTENNAS IN HEXAGONAL RINGS 
+          for k in xrange(hex_dim):
+              elements = elements + (k+1)*6
+          
+          #CREATING HEXAGONAL LAYOUT STARTING FROM CENTER      
+          ant_x = np.zeros((elements,),dtype=float)
+          ant_y = np.zeros((elements,),dtype=float)
+          
+          x = 0.0
+          y = 0.0
+
+          counter = 0
+        
+          for k in xrange(side):
+              x_row = x
+              y_row = y
+              for i in xrange(ant_main_row):
+                  if k == 0:
+                     ant_x[counter] = x_row 
+                     ant_y[counter] = y_row
+                     x_row = x_row + l
+                     counter = counter + 1 
+                  else:
+                     ant_x[counter] = x_row
+                     ant_y[counter] = y_row
+                     counter = counter + 1
+                   
+                     ant_x[counter] = x_row
+                     ant_y[counter] = -1*y_row
+                     x_row = x_row + l
+                     counter = counter + 1   
+              x = x + l/2.0
+              y = y + (np.sqrt(3)/2.0)*l                 
+              ant_main_row = ant_main_row - 1
+       
+          #RESORTING ANTENNA INDICES SO THAT LOWER LEFT CORNER BECOMES THE FIRST ANTENNA
+          y_idx = np.argsort(ant_y)
+          ant_y = ant_y[y_idx]
+          ant_x = ant_x[y_idx]
+
+          slice_value = int(side)
+          start_index = 0
+          add = True
+          ant_main_row = int(side + hex_dim)
+
+          for k in xrange(ant_main_row):
+              temp_vec_x = ant_x[start_index:start_index+slice_value]
+              x_idx = np.argsort(temp_vec_x)
+              temp_vec_x = temp_vec_x[x_idx]
+              ant_x[start_index:start_index+slice_value] = temp_vec_x
+              if slice_value == ant_main_row:
+                 add = False
+              start_index = start_index+slice_value 
+            
+              if add:
+                 slice_value = slice_value + 1
+              else:
+                 slice_value = slice_value - 1  
+
+          #SHIFTING CENTER OF ARRAY TO ORIGIN
+          max_x = np.amax(ant_x)
+                    
+          ant_x = ant_x - max_x/2.0
+          
+          temp_ant = np.zeros((len(ant_x),3),dtype=float)
+          temp_ant[:,0] = ant_x
+          temp_ant[:,1] = ant_y
+          return temp_ant
+
+      ######################################
+      #Converts the antenna idices pq into a redundant index
+      #   
+      #INPUTS:
+      #red_vec_x - the current list of unique redundant baseline vector (x-coordinate)
+      #red_vec_y - the current list of unique redundant baseline vector (y-coordinate)
+      #ant_x_p - the x coordinate of antenna p
+      #ant_x_q - the x coordinate of antenna q
+      #ant_y_p - the y coordinate of antenna p
+      #ant_y_q - the y coordinate of antenna q
+      #
+      #RETURNS:
+      #red_vec_x - the current list of unique redundant baseline vector (x-coordinate)
+      #red_vec_y - the current list of unique redundant baseline vector (y-coordinate)
+      #l - The redundant index associated with antenna p and q
+      ######################################
+      def determine_phi_value(self,red_vec_x,red_vec_y,ant_x_p,ant_x_q,ant_y_p,ant_y_q):
+          red_x = ant_x_q - ant_x_p
+          red_y = ant_y_q - ant_y_p
+
+          for l in xrange(len(red_vec_x)):
+              if (np.allclose(red_x,red_vec_x[l]) and np.allclose(red_y,red_vec_y[l])):
+                 return red_vec_x,red_vec_y,int(l+1)
+
+          red_vec_x = np.append(red_vec_x,np.array([red_x]))
+          red_vec_y = np.append(red_vec_y,np.array([red_y]))
+          return red_vec_x,red_vec_y,int(len(red_vec_x)) 
+
+      ######################################
+      #Returns the mapping phi, from pq indices to redundant indices.
+      #INPUTS:
+      #ant_x - vector containing the x-positions of all the antennas
+      #ant_y - vector containing the y-positions of all the antennas 
+      #
+      #RETURNS:
+      #phi - the mapping from pq indices to redundant indices
+      #zeta - the symmetrical counterpart
+      #L - maximum number of redundant groups  
+      ######################################
+      def calculate_phi(self,ant_x,ant_y):
+          phi = np.zeros((len(ant_x),len(ant_y)),dtype=int)
+          zeta = np.zeros((len(ant_x),len(ant_y)),dtype=int)
+          red_vec_x = np.array([])
+          red_vec_y = np.array([])
+          for k in xrange(len(ant_x)):
+              for j in xrange(k+1,len(ant_x)):
+                  red_vec_x,red_vec_y,phi[k,j]  = self.determine_phi_value(red_vec_x,red_vec_y,ant_x[k],ant_x[j],ant_y[k],ant_y[j])           
+                  zeta[k,j] = phi[k,j]
+                  zeta[j,k] = zeta[k,j]
+          L = np.amax(zeta)
+          return phi,zeta,L
+
+      ##################
+      
+      def flag_basic_all(self,ant_strcom):
           
           os.chdir(it.PATH_DATA)
+
+          file_names = glob.glob("*U.ms") 
+          #print file_names
+          #print file_names[0]
           
           #FLAG BAD CHANNELS
           ##################
@@ -164,9 +330,10 @@ class redpipe():
           options["action"]='apply'
           options["datacolumn"]='DATA'
           options["spw"] = FLAG_SPW_STRING  
-          
-
+        
           for file_name in glob.glob("*U.ms"):
+          #for file_name in np.array([file_names[0]]): # single MS      
+              print 'FLAG BAD CHANNELS\n'    
               options["vis"]=file_name
               self.flagdata_wrapper(options=options)
 
@@ -179,6 +346,8 @@ class redpipe():
           options["antenna"] = FLAG_ANT_STRING  
 
           for file_name in glob.glob("*U.ms"):
+          #for file_name in np.array([file_names[0]]): # single MS      
+              print 'FLAG BAD ANT\n'  
               options["vis"]=file_name
               self.flagdata_wrapper(options=options)
 
@@ -191,10 +360,33 @@ class redpipe():
           options["autocorr"] = True  
 
           for file_name in glob.glob("*U.ms"):
+          #for file_name in np.array([file_names[0]]): # single MS      
+              print 'FLAG AUTO\n'
               options["vis"]=file_name
               self.flagdata_wrapper(options=options)
           
+          
+          # FLAG ABOVE SOME THRESHOLD (ADDED)
+          ########################################
+         
+          options={} 
+          options["mode"]='rflag'
+          options["timedevscale"]=10.0
+          options["freqdevscale"]=5.0
+          options["datacolumn"]='DATA'
+          options["combinescans"] = True
+
+          for file_name in glob.glob("*U.ms"): # All MS
+          #for file_name in np.array([file_names[0]]): # single MS
+              print 'FLAG ABOVE SOME THRESHOLD\n'
+              options["vis"]=file_name  
+              for antstr in range(len(ant_strcom)):
+                  print ant_strcom[antstr]  
+                  options["antenna"]=ant_strcom[antstr]  
+                  self.flagdata_wrapper(options=options)
+              
           os.chdir(it.PATH_CODE)
+          
 
       def flag_aoflagger(self):
           
@@ -278,7 +470,8 @@ class redpipe():
           #BANDPASS CALIBRTION OF GC-MS WITH PS AT GALACTIC CENTER
           options={}
           options["vis"]=gc_name
-          options["solint"]='inf'
+          #options["solint"]='inf'
+	  options["solint"]='inf,3ch'# infinite in time, 3ch pre-averaged
           options["combine"]='scan'
           options["fillgaps"] = 10
           options["minsnr"] = 3
@@ -719,6 +912,7 @@ class redpipe():
 
 def main(argv):
    red_object = redpipe()
+   findrendgr = False
    flagallbasic = False
    bandpassgc = False
    plotcalgc = False
@@ -733,16 +927,17 @@ def main(argv):
    saveflags = False
 
    try:
-      opts, args = getopt.getopt(argv,"hd",["flag_all_basic","flag_ao","save_flags","bandpass_gc","plot_cal_gc","apply_cal_gc_all","create_images=","print_lst","convert_to_fits=","decon_mask"])
+      opts, args = getopt.getopt(argv,"hd",["find_red_gr","flag_all_basic","flag_ao","save_flags","bandpass_gc","plot_cal_gc","apply_cal_gc_all","create_images=","print_lst","convert_to_fits=","decon_mask"])
    except getopt.GetoptError:
-      print 'python redpipe.py -d --flag_all_basic --flag_ao --save_flags --bandpass_gc --plot_cal_gc --apply_cal_gc_all --create_images <value> --print_lst --convert_to_fits <value> --decon_mask'
+      print 'python redpipe.py -d --find_red_gr --flag_all_basic --flag_ao --save_flags --bandpass_gc --plot_cal_gc --apply_cal_gc_all --create_images <value> --print_lst --convert_to_fits <value> --decon_mask'
       sys.exit(2)
    for opt, arg in opts:
       #print "opt = ",opt
       #print "arg = ",arg
       if opt == '-h':
-         print 'python redpipe.py -d --flag_all_basic --save_flags --bandpass_gc --plotcal_gc --apply_cal_gc_all --create_images --print_lst --convert_to_fits --decon_mask'
+         print 'python redpipe.py -d --find_red_gr --flag_all_basic --save_flags --bandpass_gc --plotcal_gc --apply_cal_gc_all --create_images --print_lst --convert_to_fits --decon_mask'
          print '-d: adds a delay calibration step before the bandpass'
+         print '--find_red_gr: find redundant groups for HERA-19'
          print '--flag_all_basic: flag known bad channels, autocorrelations and antenna'
          print '--flag_ao: flag with ao flagger using strategy zen.2457545.48707.xx_strategy.rfis'
          print '--save_flags: save the flags to disk'
@@ -757,6 +952,8 @@ def main(argv):
          sys.exit()
       elif opt == '-d':
            delay = True
+      elif opt == "--find_red_gr":
+           findrendgr = True     
       elif opt == "--flag_all_basic":
            flagallbasic = True
       elif opt == "--flag_ao":
@@ -785,8 +982,10 @@ def main(argv):
            #print "HALLO"
            decon = True
 
+   if findrendgr:
+      antno=red_object.find_redundant_groups_all()   
    if flagallbasic:
-      red_object.flag_basic_all()
+      red_object.flag_basic_all(antno)
    if flagao:
       red_object.flag_aoflagger()
    if saveflags:
@@ -814,7 +1013,9 @@ if __name__ == "__main__":
    #red_object.produce_decon_mask()
    #red_object.create_images(mask = "C")
    #red_object.convert_to_fits(mask = "C")
-   #red_object.flag_basic_all()
+   #antno = red_object.find_redundant_groups_all() 
+   #print antno
+   #red_object.flag_basic_all(antno)
    #print red_object.print_lst(print_values=True)
    #red_object.bandpass_gc()
    #red_object.plot_cal_gc()
